907650e6a9626b83c5ea21f461e299a571a1a7e5
jihadmahfuz <jihadmah166@gmail.com>
2022-11-09 23:28:20 +0600

    added footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import HomeSection from '../HomeSection/HomeSection';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <HomeSection></HomeSection>
         </div>
     );
 };
